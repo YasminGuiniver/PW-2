11333954a6f8467871ef48a663f2ce770e326bce
Yasmin Guiniver <94757345+YasminGuiniver@users.noreply.github.com>
2023-03-03 15:45:34 -0300

    Delete controller directory

--- controller/global.php
+++
@@ -1,9 +0,0 @@
-<?php
-   spl_autoload_register('carregarClasseController');
-
-   function carregarClasseController ($nomeclasse){
-    if (file_exists ('../models/' .$nomeclasse. '.php')){
-        require_once '../models/' .$nomeclasse. '.php';
-    }
-   }
-?>
