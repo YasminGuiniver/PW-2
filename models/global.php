a3b99fb51b3d2d1ca6e576fdf09660d1ada9c7e4
Yasmin Guiniver <94757345+YasminGuiniver@users.noreply.github.com>
2023-03-03 15:45:50 -0300

    Delete models directory

--- models/global.php
+++
@@ -1,9 +0,0 @@
-<?php
-   spl_autoload_register('carregarClasseModel');
-
-   function carregarClasseModel ($nomeclasse){
-    if (file_exists ($nomeclasse. '.php')){
-        require_once $nomeclasse. '.php';
-    }
-   }
-?>
